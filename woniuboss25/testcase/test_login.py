3f993d7f45a07978d98b541b0d48fe4860fb3924
lifei <17710414056@163.com>
2020-03-26 19:00:25 +0800

    debug： “ ResourceWarning: unclosed <socket.socket fd=288, family=AddressFamily.AF_INET, type”

--- woniuboss25/testcase/test_login.py
+++ woniuboss25/testcase/test_login.py
@@ -1,3 +1,5 @@
+import ssl
+
 import requests, time
 import unittest
 from parameterized import parameterized
@@ -11,12 +13,19 @@ data_config_info = Utility.get_json(data_config_path)
 login_info = Utility.get_excel_to_tuple(data_config_info[0])
 
 class LoginTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.login = Login();
+
     @parameterized.expand(login_info)
     def test_login(self,url,method,test_data,resp_code,resp_content):
 
-        login_resp = Login().do_login(url, test_data)
+        login_resp =self.login.do_login(url, test_data)
         login_resp_content = login_resp.text
         self.assertEqual(login_resp_content,resp_content)
 
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+    issubclass(ssl.SSLEOFError, Exception)
